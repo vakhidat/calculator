64ee59709b08c1d080b13547ff7d5fdf2201eca6
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-17 17:15:48 +0600

    [imp] put in order methods in AdditionTest

--- src/test/java/com/epam/tat/module4/test/arithmetic_operation/AdditionTest.java
+++ src/test/java/com/epam/tat/module4/test/arithmetic_operation/AdditionTest.java
@@ -15,37 +15,42 @@ public class AdditionTest {
         calculator = new Calculator();
     }
 
-    @Test(groups = "smoke", dataProvider = "twoLongPositive", dataProviderClass = ArithmeticOperationDataProvider.class)
+    @Test(groups = "smoke",
+            dataProvider = "twoLongPositive",
+            dataProviderClass = ArithmeticOperationDataProvider.class)
     public void addTwoLongPositive(long summand1, long summand2, long sum) {
         assertEquals(calculator.sum(summand1, summand2), sum, "Summands: " + summand1 + ", " + summand2);
     }
 
-    @Test
+    @Test(dependsOnGroups = "smoke")
     public void addLongPositiveAndNegative() {
         assertEquals(calculator.sum(15L, -5L), 10L, "Sum of 15 and -5");
     }
 
-    @Test
+    @Test(dependsOnGroups = "smoke")
     public void addTwoLongNegative() {
         assertEquals(calculator.sum(-5L, -5L), -10L, "Sum of -5 and -5");
     }
 
-    @Test
+    @Test(dependsOnGroups = "smoke")
     public void addPositiveLongAndZero() {
         assertEquals(calculator.sum(10L, 0), 10L, "Sum of 10 and 0");
     }
 
-    @Test(groups = "smoke", dataProvider = "twoDoublePositive", dataProviderClass = ArithmeticOperationDataProvider.class)
+    @Test(groups = "smoke",
+            dataProvider = "twoDoublePositive",
+            dataProviderClass = ArithmeticOperationDataProvider.class,
+    dependsOnMethods = "addTwoLongPositive")
     public void addTwoDoublePositive(double summand1, double summand2, double sum) {
         assertEquals(calculator.sum(summand1, summand2), sum, "Summands: " + summand1 + ", " + summand2);
     }
 
-    @Test
+    @Test(dependsOnGroups = "smoke")
     public void addTwoDoubleNegative() {
         assertEquals(calculator.sum(-5.1, -5.1), -10.2, "Sum of -5.1 and -5.1");
     }
 
-    @Test
+    @Test(dependsOnGroups = "smoke")
     public void addPositiveDoubleAndZero() {
         assertEquals(calculator.sum(10.2, 0), 10.2, "Sum of 10.2 and 0");
     }
